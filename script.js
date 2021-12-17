6a6d42c9d7eecfa4dfef8068b02b7f06ee860dc8
Amit <64272430+amit512@users.noreply.github.com>
2021-12-17 05:42:38 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', function () {
+    let nav = document.querySelector('nav');
+    let windowPosition = window.scrollY > 0;
+    nav.classList.toggle('scrolling-active', windowPosition);
+})
+
+
+
+function myFunction() {
+    alert("Your message has been sent..");
+  }
+  function myfunction1() {
+    alert("Sorry for that but\nPage is under develop");
+  }
+
+  function myfunction2() {
+    alert("Sorry for that but\nPage is under develop");
+  }
